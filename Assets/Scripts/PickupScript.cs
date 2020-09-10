93af8aebb05343689a8f103960cef83ed228b37f
TheTastyGravy <cnpowerman23@gmail.com>
2020-09-10 16:26:50 +1000

    Add pickups, modify HUD
    Pickups are used by the PlayerManager script, so they cant currently be
    used, but should be working. The basic pickup prefab has been created
    reguardless.
    
    The info panel on the HUD has been made slightly smaller.

---
+++ Assets/Scripts/PickupScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class PickupScript : MonoBehaviour
+{
+    [SerializeField, Min(0)] protected float health;
+    [SerializeField, Min(0)] protected int ammo;
+
+
+    /// <summary>
+    /// Collect the pickup
+    /// </summary>
+    /// <param name="collector"> The object that has collected the pickup </param>
+    /// <returns> Returns true if the pickup object can be destroyed </returns>
+    public virtual bool Pickup(GameObject collector)
+    {
+        // Try to give resources to the entity
+        collector.SendMessage("Heal", health);
+        collector.SendMessage("CollectAmmo", ammo);
+
+        // Pickup collected, and can be destroyed
+        return true;
+    }
+}
