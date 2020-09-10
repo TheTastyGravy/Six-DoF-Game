464bd9403bf344f2273f29804377fff0e9b8050b
TheTastyGravy <cnpowerman23@gmail.com>
2020-09-10 14:47:50 +1000

    Add save game system
    The SaveGameScript is used to save game data held in its public struct
    SaveData. This data is saved and loaded from a file located at a
    persistant directory. It has an event that is triggered when its data
    needs to be updated before saving, avalable in the inspector.
    
    Save points are objects that act as checkpoints in a level. They only
    have a SavePointScript, deriving from Interactable, that will update the
    save's savePoint and save the current state to file.
    
    PlayerManager, which has not yet been added, will set the player's
    position to the save point with the same index as the save file.

---
+++ Assets/Scripts/SaveGameScript.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class SaveGameScript : MonoBehaviour
+{
+    /// <summary>
+    /// Data structure holds all data that needs to be saved
+    /// </summary>
+    [System.Serializable]
+    public struct SaveData
+    {
+        public float health;
+        public int ammo;
+        [HideInInspector] public int savePoint;
+        [HideInInspector] public int level;
+    }
+
+    /// <summary>
+    /// Data loaded from and saved to the save file
+    /// </summary>
+    [HideInInspector] public static SaveData saveData;
+    private string savePath;
+
+#pragma warning disable 0649
+    [SerializeField] Text notificationText;
+
+    [Tooltip("The default values used for new games")]
+    [SerializeField] private SaveData defaultValues;
+#pragma warning restore 0649
+
+    [Space]
+    /// <summary>
+    /// Invoked when the save data needs to be updated
+    /// </summary>
+    public UnityEvent gameSaveEvent;
+
+
+
+    void Awake()
+    {
+        savePath = Application.persistentDataPath + "/save.dat";
+
+        // Try to load data file
+        if (!LoadDataFromDisk())
+        {
+            Debug.Log("No save file exists; Using default values");
+            ResetToDefault();
+        }
+
+
+        // Update default values
+        defaultValues.savePoint = 0;
+        defaultValues.level = saveData.level;
+    }
+
+
+    /// <summary>
+    /// Reset the save data to the default values
+    /// </summary>
+    public void ResetToDefault()
+    {
+        saveData = defaultValues;
+    }
+
+    /// <summary>
+    /// Saves the current save data to the disk
+    /// </summary>
+    /// <param name="updateData"> Should the data be updated before saving? </param>
+    public void SaveDataToDisk(bool updateData = true)
+    {
+        if (updateData)
+        {
+            gameSaveEvent.Invoke();
+        }
+
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(savePath);
+        bf.Serialize(file, saveData);
+        file.Close();
+
+        if (notificationText != null)
+        {
+            // Show notification for 1 second
+            notificationText.enabled = true;
+            notificationText.text = "Game Saved";
+            Invoke("HideNotification", 1f);
+        }
+    }
+    /// <summary>
+    /// Loads the save data from the disk
+    /// </summary>
+    /// <returns> Was the data loaded sucessfuly? </returns>
+    public bool LoadDataFromDisk()
+    {
+        if (File.Exists(savePath))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(savePath, FileMode.Open);
+            saveData = (SaveData)bf.Deserialize(file);
+            file.Close();
+            return true;
+        }
+
+        return false;
+    }
+
+
+    void HideNotification()
+    {
+        notificationText.enabled = false;
+    }
+}
---
+++ Assets/Scripts/SavePointScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavePointScript : Interactable
+{
+    SaveGameScript saveScript;
+
+    [Tooltip("This value must be different for each save point")]
+    [Min(0)] public int index;
+
+
+    void Awake()
+    {
+        // Get the save script
+        saveScript = FindObjectOfType<SaveGameScript>();
+    }
+
+
+    public override void Interaction()
+    {
+        // Update save point
+        SaveGameScript.saveData.savePoint = index;
+        // Save data, updating the player's health and ammo
+        saveScript.SaveDataToDisk(true);
+    }
+}
