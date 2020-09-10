0ae19f2bd4c257d70e78e57fb0204525df69283a
TheTastyGravy <cnpowerman23@gmail.com>
2020-09-11 09:41:37 +1000

    Add shield and win zone
    The shield is just anobject with a trigger and "Shield" tag, but is also
    given a custom shader with backface culling off so the inside is
    rendered. ProjectileScript checks for triggers with this tag to collide
    with, so it doesnt need to do anything.
    
    The win zone is just a trigger with a WinZoneScript, without even a mesh
    or mesh renderer, because it doesnt need to be visible. When the player
    enters the trigger, it will cause a game over with the win state.

---
+++ Assets/Scripts/WinZoneScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class WinZoneScript : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Win();
+        }
+    }
+
+
+    public void Win()
+    {
+        FindObjectOfType<PauseManager>().GameOver(true);
+    }
+}
