f5a5304f1fd9139a932df349e1ef76e24cf55003
TheTastyGravy <cnpowerman23@gmail.com>
2020-09-10 14:24:44 +1000

    Add basic interaction system
    The basic system has the PlayerInteract script on the player, with
    interactable objects having a script inheriting from Interactable,
    overriding Interaction. PlayerInteract uses an event that Interactable
    adds a listener to while the player is within its trigger.
    
    The mobile controls are currently broken, as the interaction button is
    the second button, with shooting having the first, which isnt
    implemented yet.

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public abstract class Interactable : MonoBehaviour
+{
+    protected virtual void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Add listener for interaction
+            other.gameObject.GetComponent<PlayerInteract>().interact.AddListener(Interaction);
+        }
+    }
+    protected virtual void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Remove listener for interaction
+            other.gameObject.GetComponent<PlayerInteract>().interact.RemoveListener(Interaction);
+        }
+    }
+
+
+    /// <summary>
+    /// This function is called when the player interacts with the attached object
+    /// </summary>
+    public abstract void Interaction();
+}
---
+++ Assets/Scripts/Player Scripts/PlayerInteract.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerInteract : MonoBehaviour
+{
+    /// <summary>
+    /// Invoked when the player attempts to interact with something.
+    /// If something is interactable, it should add a listener to this event on trigger enter, and remove it on trigger exit
+    /// </summary>
+    [HideInInspector] public UnityEvent interact;
+
+    MobileControllerScript mobileScript;
+
+
+    void Awake()
+    {
+        // Get mobile controller script
+        mobileScript = GameObject.Find("Mobile Controller").GetComponent<MobileControllerScript>();
+    }
+    void Start()
+    {
+        if (mobileScript.onMobile)
+        {
+            // Create new touch button at the bottom right
+            mobileScript.CreateNewButton(new Vector2(-150, 250), new Vector2(1, 0), new Vector2(1, 0), new Vector2(1, 0), 60);
+        }
+    }
+
+
+    bool canPress = true;
+    void Update()
+    {
+        // Get input. Either use touch button for mobile, or axis for mouse and controller
+        float input;
+        if (mobileScript.onMobile)
+            input = (mobileScript.touchButtonDown[1]) ? 1 : 0;
+        else
+            input = Input.GetAxisRaw("Interact");
+
+        // On button down, invoke the interact event
+        if (input == 1f && canPress)
+        {
+            interact.Invoke();
+
+            canPress = false;
+        }
+        if (input == 0f && !canPress)
+        {
+            canPress = true;
+        }
+    }
+}
