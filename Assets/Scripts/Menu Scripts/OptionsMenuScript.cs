7af296d5a492e338a74af27f4748a7cee732e632
TheTastyGravy <cnpowerman23@gmail.com>
2020-09-11 11:50:47 +1000

    Add options menu
    The options menu is accesable from the main menu, and provides two
    things: settings and controls. Settings can be set using sliders and
    toggles to set PlayerPrefs used in the game. Controls can be viewed by
    pressing a button, displaying the appropriately labeled control scheme.
    
    The settings are all operational without issue. The controls sellection
    works, however the controls displayed are only placeholder and need to
    be changed.

---
+++ Assets/Scripts/Menu Scripts/OptionsMenuScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class OptionsMenuScript : MonoBehaviour
+{
+    public Toggle invertY;
+
+
+    void Awake()
+    {
+        // Set the toggle to the current player pref and add listener
+        invertY.isOn = PlayerPrefs.GetInt("InvertY", 0) == 1;
+        invertY.onValueChanged.AddListener(new UnityAction<bool>(InvertYChange));
+    }
+
+
+    public void ReturnButtonClick()
+    {
+        // Save player prefs to disc
+        PlayerPrefs.Save();
+        // Return to main menu
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+    public void InvertYChange(bool value)
+    {
+        // Update player prefs
+        PlayerPrefs.SetInt("InvertY", value ? 1 : 0);
+    }
+
+
+    public GameObject activePanel;
+    public void ControlsButtonClick(GameObject panel)
+    {
+        activePanel.SetActive(false);
+        activePanel = panel;
+        activePanel.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Menu Scripts/PlayerPrefsSlider.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[DisallowMultipleComponent, RequireComponent(typeof(Slider))]
+public class PlayerPrefsSlider : MonoBehaviour
+{
+    // This script requiers the slider object have:
+    //      -A Text object named 'Text'
+    //      -A InputField object named 'InputField'
+    //
+    // This script also adds its own listeners to the appropriate events
+
+    Slider slider;
+    InputField field;
+
+    public string playerPrefsKey;
+    // Used to set the label attached to the slider
+    [SerializeField] string labelText = "Label";
+
+    
+    // Called when a value in the inspector in changed
+    void OnValidate()
+    {
+        // Update label in editor
+        transform.Find("Text").GetComponent<Text>().text = labelText;
+    }
+
+    void Start()
+    {
+        // Get components
+        slider = GetComponent<Slider>();
+        field = transform.Find("InputField").GetComponent<InputField>();
+
+        // Add listeners to slider and input field events
+        slider.onValueChanged.AddListener(new UnityAction<float>(SliderChange));
+        field.onEndEdit.AddListener(new UnityAction<string>(FieldChange));
+        
+        // Set slider to current player pref
+        slider.value = PlayerPrefs.GetFloat(playerPrefsKey, 1f);
+    }
+
+
+    public void SliderChange(float newValue)
+    {
+        // Set input field to display current value, without invoking the event
+        field.SetTextWithoutNotify(newValue.ToString("F2"));
+        // Update player prefs, rounding to 2 decimals
+        PlayerPrefs.SetFloat(playerPrefsKey, Mathf.Round(newValue * 100f) / 100f);
+    }
+    public void FieldChange(string newValue)
+    {
+        // Update the slider, calling SliderChange()
+        slider.value = float.Parse(newValue);
+    }
+}
