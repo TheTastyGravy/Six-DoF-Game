4e78fccba3f9e3a57da2f83127e1a02a29768cf1
TheTastyGravy <cnpowerman23@gmail.com>
2020-09-11 10:09:57 +1000

    Add turret
    The turret has a TurretScript that controls it, alog with a requiered
    ShootScript. It has multiple children, but they can mostly be ignored.
    It will cast a ray toward its target (set to the player), and if it
    hits, aim the barrel toward them and start shooting, until it looses
    sight of them. In order to not have the ray hitting everything, a layer
    mask is used, with a new layer being added to objects it should be able
    to see through, such as the shield.

---
+++ Assets/Scripts/TurretScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ShootScript))]
+public class TurretScript : MonoBehaviour
+{
+    ShootScript shootScript;
+    LayerMask mask;
+
+    [SerializeField] Transform target;
+    [SerializeField] Transform rotationPoint;
+    [SerializeField] Transform firePoint;
+        [Space]
+    public float health = 5;
+
+    
+    void Awake()
+    {
+        // If transforms are not already set, try to set them
+        if (target == null)
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+        if (rotationPoint == null)
+            rotationPoint = transform.Find("RotationPoint");
+        if (firePoint == null)
+            firePoint = transform.Find("RotationPoint/Barrel/FirePoint");
+        
+        // Get ShootScript, which is requiered, so must exist
+        shootScript = GetComponent<ShootScript>();
+        // Create a mask to ignore the two layers
+        mask = ~LayerMask.GetMask("TurretIgnore", "Projectiles");
+    }
+
+
+    void FixedUpdate()
+    {
+        // If a raycast from the barrel hits the player
+        if (Physics.Raycast(firePoint.position, target.position - firePoint.position, out RaycastHit hit, 50, mask) && hit.transform == target)
+        {
+            // Point the barrel at the player
+            rotationPoint.LookAt(target);
+            shootScript.Shoot();
+        }
+        else
+        {
+            // Face forward
+            rotationPoint.rotation = Quaternion.identity;
+        }
+    }
+
+
+    public void DealDamage(float damage)
+    {
+        // Ignore negitive damage
+        if (damage <= 0)
+            return;
+
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
