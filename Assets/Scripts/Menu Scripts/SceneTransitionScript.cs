60523e9f70d685c610626dbcbe3188a54bc48698
TheTastyGravy <cnpowerman23@gmail.com>
2020-09-11 10:36:29 +1000

    Add level transition scene
    LevelTransitionScene should be loaded whenever a level is to be loaded.
    It reads the save file's current level index, and will load that level,
    or the first level is the index is out of range.
    
    With this addition, the play button works, and new levels can be loaded.

---
+++ Assets/Scripts/Menu Scripts/SceneTransitionScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(SaveGameScript))]
+public class SceneTransitionScript : MonoBehaviour
+{
+    void Start()
+    {
+        // Add the level value to the index of level 1 (this scene is before the first level)
+        int level = SceneManager.GetActiveScene().buildIndex + 1 + SaveGameScript.saveData.level;
+
+        // If the index is invalid, reset level
+        if (SceneManager.sceneCountInBuildSettings <= level)
+        {
+            // Revert to level 1
+            level -= SaveGameScript.saveData.level;
+
+            // Reset level settings and save to disk
+            SaveGameScript.saveData.level = 0;
+            SaveGameScript.saveData.savePoint = 0;
+            GetComponent<SaveGameScript>().SaveDataToDisk(false);
+        }
+
+        // Load the current level
+        SceneManager.LoadScene(level, LoadSceneMode.Single);
+    }
+}
