ed04e46e5f940dff46a035fcfe380bd1f1955c18
TheTastyGravy <cnpowerman23@gmail.com>
2020-09-11 10:30:39 +1000

    Add main menu
    The main menu only has a single script with methods for each of the
    buttons. Two of these methods load scenes that dont exist yet, so they
    dont currently work.

---
+++ Assets/Scripts/Menu Scripts/MainMenuScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    
+
+    public void PlayButtonClick()
+    {
+        SceneManager.LoadScene("LevelTransitionScene", LoadSceneMode.Single);
+    }
+    public void QuitButtonClick()
+    {
+        Application.Quit();
+    }
+    public void OptionButtonClick()
+    {
+        SceneManager.LoadScene("OptionsMenu", LoadSceneMode.Single);
+    }
+}
